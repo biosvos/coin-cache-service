0c15c9a85eb62de00d5d2c2639e91213cff559af
jinsu <biosvos@gmail.com>
2025-01-30 14:26:19 +0900

    refine prohibitor

--- internal/app/prohibitor/prohibitor.go
+++ internal/app/prohibitor/prohibitor.go
@@ -33,6 +33,8 @@ type Prohibitor struct {
 	scheduler gocron.Scheduler
 }
 
+const day = 24 * time.Hour
+
 func NewProhibitor(logger *zap.Logger, bus bus.Bus, repo Repository) *Prohibitor {
 	scheduler, _ := gocron.NewScheduler()
 	return &Prohibitor{logger: logger, bus: bus, repo: repo, scheduler: scheduler}
@@ -40,16 +42,11 @@ func NewProhibitor(logger *zap.Logger, bus bus.Bus, repo Repository) *Prohibitor
 
 func (p *Prohibitor) Start(ctx context.Context) error {
 	p.scheduler.Start()
-	err := p.checkAndAllowCoins(ctx)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	err = p.checkAndProhibitCoins(ctx)
-	if err != nil {
-		return errors.WithStack(err)
-	}
 	p.bus.Subscribe(ctx, domain.CoinCreatedEventTopic, p.handleCoinCreated)
 	p.bus.Subscribe(ctx, domain.CoinUpdatedEventTopic, p.handleCoinUpdated)
+	p.bus.Subscribe(ctx, domain.CoinDeletedEventTopic, p.handleCoinDeleted)
+	p.bus.Subscribe(ctx, domain.TradesUpdatedEventTopic, p.handleTradesUpdated)
+	p.bus.Subscribe(ctx, domain.TradesDeletedEventTopic, p.handleTradesDeleted)
 	return nil
 }
 
@@ -60,77 +57,66 @@ func (p *Prohibitor) Stop() {
 	}
 }
 
-func (p *Prohibitor) checkAndAllowCoins(ctx context.Context) error {
-	bannedCoins, err := p.repo.ListBannedCoins(ctx)
+func (p *Prohibitor) addExpireBannedCoinJob(ctx context.Context, bannedCoin *domain.BannedCoin) {
+	_, err := p.scheduler.NewJob(
+		gocron.OneTimeJob(
+			gocron.OneTimeJobStartDateTime(
+				bannedCoin.ExpiredAt(),
+			),
+		),
+		gocron.NewTask(
+			func(coinID domain.CoinID) {
+				bannedCoin, err := p.repo.GetBannedCoin(ctx, coinID)
+				if err != nil {
+					p.logger.Error("failed to get banned coin", zap.Error(err))
+					return
+				}
+				err = p.deleteBannedCoin(ctx, bannedCoin)
+				if err != nil {
+					p.logger.Error("failed to delete banned coin", zap.Error(err))
+				}
+			},
+			bannedCoin.CoinID(),
+		),
+	)
 	if err != nil {
-		return errors.WithStack(err)
+		p.logger.Error("failed to add expire banned coin job", zap.Error(err))
 	}
-	for _, bannedCoin := range bannedCoins {
-		err := p.checkAndAllowCoin(ctx, bannedCoin.CoinID())
-		if err != nil {
-			return errors.WithStack(err)
-		}
-	}
-	return nil
-}
-
-func (p *Prohibitor) checkAndAllowCoin(ctx context.Context, coinID domain.CoinID) error {
-	now := time.Now()
-	bannedCoin, err := p.repo.GetBannedCoin(ctx, coinID)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	if !bannedCoin.IsBanOver(now) {
-		return nil
-	}
-	err = p.repo.DeleteBannedCoin(ctx, bannedCoin)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	p.logger.Info("allowed coin", zap.String("coin_id", string(bannedCoin.CoinID())))
-	p.bus.Publish(ctx, domain.NewBannedCoinDeletedEvent(bannedCoin.CoinID()))
-	return nil
-}
-
-func (p *Prohibitor) checkAndProhibitCoins(ctx context.Context) error {
-	coins, err := p.repo.ListCoins(ctx)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	for _, coin := range coins {
-		err := p.CheckAndProhibitCoinByStatus(ctx, coin.ID())
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		err = p.CheckAndProhibitCoinByTrades(ctx, coin.ID())
-		if err != nil {
-			return errors.WithStack(err)
-		}
-	}
-	return nil
 }
 
 func (p *Prohibitor) handleCoinCreated(ctx context.Context, event domain.Event) error {
 	coinCreatedEvent := domain.ParseCoinCreatedEvent(event.Payload())
-	return p.CheckAndProhibitCoinByStatus(ctx, coinCreatedEvent.CoinID)
+	return p.prohibitByStatus(ctx, coinCreatedEvent.CoinID)
 }
 
 func (p *Prohibitor) handleCoinUpdated(ctx context.Context, event domain.Event) error {
 	coinUpdatedEvent := domain.ParseCoinUpdatedEvent(event.Payload())
-	return p.CheckAndProhibitCoinByStatus(ctx, coinUpdatedEvent.CoinID)
+	return p.prohibitByStatus(ctx, coinUpdatedEvent.CoinID)
 }
 
-const day = 24 * time.Hour
+func (p *Prohibitor) handleCoinDeleted(ctx context.Context, event domain.Event) error {
+	coinDeletedEvent := domain.ParseCoinDeletedEvent(event.Payload())
+	return p.allowCoin(ctx, coinDeletedEvent.CoinID)
+}
 
-func (p *Prohibitor) CheckAndProhibitCoinByStatus(ctx context.Context, coinID domain.CoinID) error {
-	alreadyBanned, err := p.isAlreadyBanned(ctx, coinID)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	if alreadyBanned {
+func (p *Prohibitor) handleTradesUpdated(ctx context.Context, event domain.Event) error {
+	tradesUpdatedEvent := domain.ParseTradesUpdatedEvent(event.Payload())
+	return p.prohibitByTrades(ctx, tradesUpdatedEvent.CoinID)
+}
+
+func (p *Prohibitor) handleTradesDeleted(ctx context.Context, event domain.Event) error {
+	tradesDeletedEvent := domain.ParseTradesDeletedEvent(event.Payload())
+	return p.allowCoin(ctx, tradesDeletedEvent.CoinID)
+}
+
+func (p *Prohibitor) prohibitByStatus(ctx context.Context, coinID domain.CoinID) error {
+	_, err := p.repo.GetBannedCoin(ctx, coinID)
+	if err == nil { // already banned
 		return nil
 	}
-
+	if !errors.Is(err, coinrepository.ErrBannedCoinNotFound) {
+		return errors.WithStack(err)
+	}
 	coin, err := p.repo.GetCoin(ctx, coinID)
 	if err != nil {
 		return errors.WithStack(err)
@@ -138,44 +124,35 @@ func (p *Prohibitor) CheckAndProhibitCoinByStatus(ctx context.Context, coinID do
 	if !coin.IsDanger() {
 		return nil
 	}
-
-	bannedCoin := domain.NewBannedCoin(coinID, time.Now(), day)
-
-	_, err = p.repo.CreateBannedCoin(ctx, bannedCoin)
+	err = p.createBannedCoin(ctx, coinID, day)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	p.bus.Publish(ctx, domain.NewBannedCoinCreatedEvent(coinID))
-	p.logger.Info("prohibited coin", zap.String("coin_id", string(coinID)))
 	return nil
 }
 
-func (p *Prohibitor) CheckAndProhibitCoinByTrades(ctx context.Context, coinID domain.CoinID) error {
-	alreadyBanned, err := p.isAlreadyBanned(ctx, coinID)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	if alreadyBanned {
+func (p *Prohibitor) prohibitByTrades(ctx context.Context, coinID domain.CoinID) error {
+	_, err := p.repo.GetBannedCoin(ctx, coinID)
+	if err == nil { // already banned
 		return nil
 	}
-
+	if !errors.Is(err, coinrepository.ErrBannedCoinNotFound) {
+		return errors.WithStack(err)
+	}
 	trades, err := p.repo.ListTrades(ctx, coinID)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-
 	var banDuration time.Duration
 	if !trades.IsEnoughTrade() { // 20개 이하면, 20개 이상이 될때까지 금지한다.
 		banDuration = max(banDuration, day)
 	}
-
 	lastPrice := trades.LastPrice()
 	sp := strings.Split(string(lastPrice), ".")
 	price, err := strconv.ParseUint(sp[0], 10, 64)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-
 	const maxPrice = 100_000
 	const tenDays = 10 * day
 	if maxPrice < price {
@@ -187,27 +164,50 @@ func (p *Prohibitor) CheckAndProhibitCoinByTrades(ctx context.Context, coinID do
 		banDuration = max(banDuration, tenDays)
 	}
 
-	bannedCoin := domain.NewBannedCoin(coinID, time.Now(), banDuration)
-
 	if banDuration == 0 {
 		return nil
 	}
-	_, err = p.repo.CreateBannedCoin(ctx, bannedCoin)
+	err = p.createBannedCoin(ctx, coinID, banDuration)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (p *Prohibitor) allowCoin(ctx context.Context, coinID domain.CoinID) error {
+	bannedCoin, err := p.repo.GetBannedCoin(ctx, coinID)
+	if err != nil {
+		if errors.Is(err, coinrepository.ErrBannedCoinNotFound) {
+			// already allowed
+			return nil
+		}
+		return errors.WithStack(err)
+	}
+	if !bannedCoin.IsBanOver(time.Now()) {
+		// still banned
+		return nil
+	}
+	return p.deleteBannedCoin(ctx, bannedCoin)
+}
+
+func (p *Prohibitor) createBannedCoin(ctx context.Context, coinID domain.CoinID, period time.Duration) error {
+	bannedCoin := domain.NewBannedCoin(coinID, time.Now(), period)
+	_, err := p.repo.CreateBannedCoin(ctx, bannedCoin)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	p.logger.Info("prohibited coin", zap.String("coin_id", string(coinID)))
 	p.bus.Publish(ctx, domain.NewBannedCoinCreatedEvent(coinID))
+	p.addExpireBannedCoinJob(ctx, bannedCoin)
 	return nil
 }
 
-func (p *Prohibitor) isAlreadyBanned(ctx context.Context, coinID domain.CoinID) (bool, error) {
-	_, err := p.repo.GetBannedCoin(ctx, coinID)
+func (p *Prohibitor) deleteBannedCoin(ctx context.Context, bannedCoin *domain.BannedCoin) error {
+	err := p.repo.DeleteBannedCoin(ctx, bannedCoin)
 	if err != nil {
-		if errors.Is(err, coinrepository.ErrBannedCoinNotFound) {
-			return false, nil
-		}
-		return false, errors.WithStack(err)
+		return errors.WithStack(err)
 	}
-	return true, nil
+	p.logger.Info("allowed coin", zap.String("coin_id", string(bannedCoin.CoinID())))
+	p.bus.Publish(ctx, domain.NewBannedCoinDeletedEvent(bannedCoin.CoinID()))
+	return nil
 }
